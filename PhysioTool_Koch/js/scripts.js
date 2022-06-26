e28b8ad005a3c70183e0b866e04a9f227567d111
mirikoch <101625153+mirikoch@users.noreply.github.com>
2022-06-26 06:32:42 +0200

    mongodb connect

--- PhysioTool_Koch/js/scripts.js
+++ PhysioTool_Koch/js/scripts.js
@@ -1,38 +1,277 @@
-const title = document.getElementById("shop-item-title").textContent;
-const image = document.getElementById('shop-item-image').src;
-const series = document.getElementById('series').value;
-const repetition = document.getElementById('repetition').value;
-const dauer = document.getElementById('dauer').value;
-const comment = document.getElementById('comment').value;
+// Pop-Up Details-Button
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+//Hinzufügen-Button
+// HTMLElement Definitionen
+const enterbutton = document.querySelector("#enter-button");
+const seriesInput = document.querySelector("#series");
+const repInput = document.querySelector("#repetition");
+const dauerInput = document.querySelector("#dauer");
+const commentInput = document.querySelector("#comment");
+const tableBody = document.querySelector("#table-body");
+
+// EventHandler für Enter-Button
+enterbutton.addEventListener("click", enterEvent);
+
+/**
+* Funktion für die Eventlistner der einzelnen Lösch-Buttons
+* @param {Event} evt
+* @param {HTMLElement} parentElement
+*/
+function handleDelete(evt, parentElement) {
+    tableBody.removeChild(parentElement);
+    // update data and localstorage
+    saveConcertEvents();
+}
+
+// Bauplan für die Zeile des Löschbuttons
+
+/**
+* Bauplan für die Zeile des Löschbuttons
+* @param {HTMLElement} parentElement
+* @return {HTMLElement} die Zeile des Löschbuttons
+*/
+function createDeleteButtonCell(parentElement) {
+    // lege eine neue Tabellenzelle und einen neuen Button an
+    const tablecell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    // Füllen des Buttons mit Inhalt
+    deleteButton.textContent = "Entfernen";
+    // gib dem Button die passenden HTML-Klassen
+    deleteButton.classList.add("btn-danger");
+    // Füge einen Eventlistener dem Löschbutton hinzu
+    deleteButton.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        handleDelete(evt, parentElement);
+    });
+    // füge in die Tabellenzelle den Löschbutton ein
+    tablecell.appendChild(deleteButton);
+    // gebe die Tabellenzelle zurück
+    return tablecell;
+}
+
+/**
+* Funktion erstellt eine einzelnen Tabellenzelle mit der entsprechenden Value
+* @param {string} value
+* @param {string} name
+* @return {HTMLElement} die Zeile des Löschbuttons
+*/
+function createTableCell(value, name) {
+    const tablecell = document.createElement("td");
+    tablecell.setAttribute(name, value);
+    tablecell.textContent = value;
+    return tablecell;
+}
+
+/**
+* Funktion erstellt eine einzelne Tabellenzelle mit der entsprechenden Value
+* @param {object} object das Objekt aus dem ein neues Element erstellt werden soll (optional)
+*/
+function createNewEventEntry(object) {
+    console.log("createNewEventEntry aufgerufen");
+
+    // Element für Tabellenreihe anlegen
+    const tableEntry = document.createElement("tr");
+    // Addressierung durch eine entsprechende Klasse
+    tableEntry.classList.add("table-entry");
+    let series;
+    let rep;
+    let dauer;
+    let comment;
+    // Auslesen der Input-Werte
+   
+    console.log(object);
+    if (object) {
+        series = object.series;
+        rep = object.rep;
+        dauer = object.dauer;
+        comment = object.comment;
+    } else {
+        series = Number(seriesInput.value);
+        rep = Number(repInput.value);
+        dauer = Number(dauerInput.value);
+        comment = commentInput.value;
+    }
+
+    /*
+    // Validierung der Input-Werte
+    if (!validation(series, rep, dauer, comment)) {
+        return;
+    }*/
+
+    // lege die einzelnen Tabellenzellen an
+    const cells = [
+        createTableCell(series, "series"),
+        createTableCell(rep, "repetition"),
+        createTableCell(dauer, "dauer"),
+        createTableCell(comment, "comment"),
+        createDeleteButtonCell(tableEntry)
+    ];
+
+    // füge die einzelnen Tabellenzellen der Tabellenreihe hinzu
+    for (const cell of cells) {
+        tableEntry.appendChild(cell);
+    }
+
+    // pflege die gesamte Tabellenreihe in die Tabellenstruktur ein
+    tableBody.appendChild(tableEntry);
+}
+
+
+/**
+* Funktion validiert die Eingabefelder "Interpret", "Preis" und "Datum"
+* @param {Number} series Der Wert im Serien-Eingabefeld
+* @param {Number} rep Der Wert im Wdh-Eingabefeld
+* @param {Number} dauer Der Wert im Dauer-Eingabefeld
+* @param {String} comment Der Wert im Kommentar-Eingabefeld
+* @return {Boolean} Gibt true zurück, wenn die Eingaben valide sind
+*/
+
+// EventHandler für Enter-Button
+function enterEvent(_evt) {
+    _evt.preventDefault();
+    createNewEventEntry();
+    saveConcertEvents();
+}
+
+/**
+ * Funktion zum aktualisieren des Events-Arrays im Local Storage
+ */
+ function saveConcertEvents() {
+    // suche zuächst die TabellenEinträge aus dem DOM heraus.
+    const tableEntries = document.querySelectorAll(".table-entry");
+
+    // initialisiere das Events-Array neu
+    const currentTableEntries = [];
+    // lese für jeden Tabelleneintrag die Werte aus
+    for (const entry of tableEntries) {
+        const seriesElement = entry.querySelector("[series]");
+        const repElement = entry.querySelector("[repetition]");
+        const dauerElement = entry.querySelector("[dauer]");
+        const commentElement = entry.querySelector("[comment]");
+
+        // auslesen der Werte, der einzelnen Zellen
+        const series = Number(seriesElement.dataset.series);
+        const rep = Number(repElement.dataset.rep);
+        const dauer = Number(dauerElement.dataset.dauer);
+        const comment = commentElement.dataset.comment;
+
+        // Speicher der Attribute, im Events-Array
+        currentTableEntries.push({
+            series: series,
+            rep: rep,
+            dauer: dauer,
+            comment: comment
+        });
+    }
+
+    /* Nachdem diese For-Schleife durchgelaufen ist,
+    ist das Events-Array wieder auf dem aktuellsten Stand */
+    localStorage.setItem("event_storage", JSON.stringify(currentTableEntries));
+}
+
+/**
+ * Funktion zum aktuallisieren der Tabelle
+ */
+function updateView() {
+    // Hole die Daten aus dem LocalStorage
+    const storageString = localStorage.getItem("event_storage");
+    // konvertiere die Daten zurück in ein Objekt und ersätz damit das Events-Array
+    const currentTableEntries = JSON.parse(storageString);
+    // schaue zunächst ob es Einträge gibt, wenn Nein, springe aus der Funktion
+    if (!currentTableEntries) {
+        return;
+    }
+    // interiere die Tabellenelemente und erstelle für jedes Element einen Tabelleneintrag
+    for (const element of currentTableEntries) {
+        createNewEventEntry(element);
+    }
+}
+
+/**
+ * Init Funktion aktualisiert die Ansicht
+ */
+function init() {
+    updateView();
+}
+
+// Init-Funktion aufrufen
+init();
+
+
+const title = document.getElementById("shop-item-title");
+const image = document.getElementById('shop-item-image');
+const series = document.getElementById('series');
+const repetition = document.getElementById('repetition');
+const dauer = document.getElementById('dauer');
+const comment = document.getElementById('comment');
 
 let exercises = [];
 
 
 const addExercise = (e) => {
-    console.log("run");
+    console.log("addExcercise() wurde aufgerufen");
     e.preventDefault();
 
+    const titleInput = title.textContent;
+    const imageInput = image.src;
+    const seriesInput = series.value;
+    const repetitionInput = repetition.value;
+    const dauerInput = dauer.value;
+    const commentInput = comment.value;
+
     let exercise = {
-        title: title,
-        image: image,
-        series: series,
-        repetition: repetition,
-        dauer: dauer,
-        comment: comment
+        title: titleInput,
+        image: imageInput,
+        series: seriesInput,
+        repetition: repetitionInput,
+        dauer: dauerInput,
+        comment: commentInput
     };
     console.log(exercise);
 
     sendJSONStringWithPOST(
-        'http://localhost:3000/',
+        'http://localhost:3000/plan',
         JSON.stringify(exercise)
     );
 
     exercises.push(exercise);
     document.querySelector('input').reset;
-
-    let cart = document.querySelector('#cart');
-    cart.textContent = '\n' + JSON.stringify(exercises, '\t',2);
-
 
     // send json data to localstorage
     localStorage.setItem('ExerciseArray', JSON.stringify(exercises));
@@ -54,27 +293,27 @@ function displayExercise() {
           <span class="dropdown-cart-item-title"> 
             ${document.getElementById('shop-item-title').textContent} </span>
           </div>
-                  
+
           <div class="cart-quantity cart-column">
               <button class="btn btn-danger" type="button">ENTFERNEN</button>
           </div> 
       `;
         plan.innerHTML += plan;
     }
-}
+} 
 
 
 const btns = document.getElementsByClassName('shop-item-button');
 
 for (btn of btns) {
-    btn.addEventListener('click', addExercise);
+    btn.addEventListener("click", addExercise);
 }
 
 
 // Server-Funktionen
 async function sendJSONStringWithPOST(url, jsonString) {
     const response = await fetch(url, {
-        method: 'post',
+        method: "post",
         body: jsonString,
     });
     const text = await response.text();
--- PhysioTool_Koch/js/server.js
+++ PhysioTool_Koch/js/server.js
@@ -1,34 +1,65 @@
-const http = require('http');
+const http = require("http");
+const mongodb = require("mongodb");
 
-const hostname = '127.0.0.1'; // localhost
+const hostname = "127.0.0.1"; // localhost
 const port = 3000;
+const url = "mongodb://localhost:27017"; // für lokale MongoDB
+const mongoClient = new mongodb.MongoClient(url);
 
-const server = http.createServer((request, response) => {
+async function startServer() {
+    // connect to database
+    await mongoClient.connect();
+    // listen for requests
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+}
+
+const server = http.createServer(async (request, response) => {
     response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json");
-    response.setHeader('Access-Control-Allow-Origin', '*'); // bei CORS Fehler 
-    const url = new URL(request.url || '', `http://${request.headers.host}`);
+    response.setHeader("Access-Control-Allow-Origin", "*"); // bei CORS Fehler
+    let url = new URL(request.url || " ", `http://${request.headers.host}`);
     switch (url.pathname) {
-      case '/':
-        if (request.method === 'POST') {
-          let jsonString = '';
-          request.on('data', (data) => {
-            jsonString += data;
-          });
-          request.on('end', () => {
-            console.log(JSON.parse(jsonString));
-            response.end(JSON.stringify(jsonString));
-          });
+    case "/plan": {
+        console.log("/plan wurde aufgerufen");
+        const planCollection = mongoClient.db("physiotool").collection("plan");
+        switch (request.method) {
+        case "GET":
+            let result;
+            if (url.searchParams.get("series")) {
+                result = await planCollection.find({
+                    series: Number(url.searchParams.get("series"))
+                });
+            }
+            else {
+                result = await planCollection.find({});
+            }
+            response.setHeader("Content-Type", "application/json");
+            response.write(JSON.stringify(result.toArray()));
+            break;
+        case "POST":
+            let jsonString = " ";
+            request.on("data", data => {
+                jsonString += data;
+            });
+            request.on("end", async () => {
+                console.log(JSON.parse(jsonString));
+                planCollection.insertOne(JSON.parse(jsonString));
+            });
+            break;
         }
         break;
-      case '/search':
-        response.write('Hier ist was du suchst: ' + url.searchParams.get('item'));
-      default:
+    }
+
+    case "/clearAl":
+        await mongoClient.db("physiotool").collection("plan").drop();
+        break;
+    default:
         response.statusCode = 404;
     }
-  });
+    response.end();
+}
+);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+startServer();
 
